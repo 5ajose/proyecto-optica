97a3aefe0b16818fbe0221defb7a59738d613102
5ajose <jaquintoaguilar@gmail.com>
2024-11-28 16:11:37 -0500

    cat

--- JavaScript/catalogo.js
+++ JavaScript/catalogo.js
@@ -1,16 +1,16 @@
-body.style.backGround = "Red"
+// body.style.backGround = "Red"
 
-let enviarInputs = document.getElementById("enviar")
-enviarInputs.addEventListener("click",(e)=>{
+// let enviarInputs = document.getElementById("enviar")
+// enviarInputs.addEventListener("click",(e)=>{
     
-    let imagen = document.getElementById("imagen")
-    let marca = document.getElementById("marca")
-    let precio = document.getElementById("precio")
-    console.log(marca.value);
+//     let imagen = document.getElementById("imagen")
+//     let marca = document.getElementById("marca")
+//     let precio = document.getElementById("precio")
+//     console.log(marca.value);
     
 
-    let crearElemento = document.getElementById("p")
-    crearElemento.textContent = marca
-    crearElemento.textContent = precio
+//     let crearElemento = document.getElementById("p")
+//     crearElemento.textContent = marca
+//     crearElemento.textContent = precio
     
-})
+// })
